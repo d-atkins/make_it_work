01eea2226d3f0cadf9fb24d5a0222df7765ae798
David Atkins <atki1080@gmail.com>
2020-02-07 10:13:50 -0700

    Add test for adding contestant to project

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -17,8 +17,36 @@ RSpec.describe 'As a visitor' do
       expect(page).to have_content("Litfit")
       expect(page).to have_content("Material: Lamp Shade")
       expect(page).to have_content("Challenge Theme: Apartment Furnishings")
-      expect(page).to have_content("Number of Contestants: #{project1.number_of_contestants}")
-      expect(page).to have_content("Average Contestant Experience: #{project1.average_contestant_experience.round(2)}")
+      expect(page).to have_content("Number of Contestants: 3")
+      expect(page).to have_content("Average Contestant Experience: 10.33")
+    end
+
+    it "I can add a contestant to a project" do
+      challenge1 = Challenge.create!(theme: "Apartment Furnishings", project_budget: 50)
+      project1 = Project.create!(name: "Litfit", material: "Lamp Shade", challenge: challenge1)
+      contestant1 = Contestant.create!(name: "Kentaro Kameyama", age: 32, hometown: "Denver, CO", years_of_experience: 10)
+      contestant2 = Contestant.create!(name: "Jay McCarroll", age: 37, hometown: "Austin, TX", years_of_experience: 15)
+      contestant3 = Contestant.create!(name: "Ambrose Lau", age: 27, hometown: "San Diego, CA", years_of_experience: 6)
+      contestant4 = Contestant.create!(name: "New Guy", age: 12, hometown: "Longmont, CO", years_of_experience: 1)
+
+      project1.contestants << contestant1
+      project1.contestants << contestant2
+      project1.contestants << contestant3
+
+      visit "/projects/#{project1.id}"
+
+      expect(page).to have_content("Number of Contestants: 3")
+
+      fill_in "contestant_id", with: contestant4.id
+      click_button "Add Contestant To Project"
+
+      expect(page).to have_content("Number of Contestants: 4")
+
+      visit "/contestants"
+
+      within("#contestant-#{contestant4.id}") do
+        expect(page).to have_content("Projects: #{project1.name}")
+      end
     end
   end
 end
