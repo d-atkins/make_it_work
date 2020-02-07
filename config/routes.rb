2b9d20c9b03887002143829686d13c08f82261a3
David Atkins <atki1080@gmail.com>
2020-02-07 10:23:08 -0700

    Add route to update project's contestants

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/projects/:project_id', to: 'projects#show'
+  patch '/projects/:project_id', to: 'project_contestants#update'
+
   get '/contestants', to: 'contestants#index'
 end
