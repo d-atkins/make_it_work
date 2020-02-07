6944dbc20cb1c8790bdc2851fc11cc6c369ebeea
David Atkins <atki1080@gmail.com>
2020-02-07 09:25:39 -0700

    Change variable names

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe 'As a visitor' do
   describe 'from the project show page' do
     it "I can see that project's name, material, and theme" do
-      c1 = Challenge.create!(theme: "Apartment Furnishings", project_budget: 50)
-      p1 = Project.create!(name: "Litfit", material: "Lamp Shade", challenge: c1)
+      challenge1 = Challenge.create!(theme: "Apartment Furnishings", project_budget: 50)
+      project1 = Project.create!(name: "Litfit", material: "Lamp Shade", challenge: challenge1)
 
-      visit "/projects/#{p1.id}"
+      visit "/projects/#{project1.id}"
       expect(page).to have_content("Litfit")
       expect(page).to have_content("Material: Lamp Shade")
       expect(page).to have_content("Challenge Theme: Apartment Furnishings")
