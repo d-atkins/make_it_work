094b545d7c82c84d02f63ebdc490920e6460148d
David Atkins <atki1080@gmail.com>
2020-02-07 11:09:16 -0700

    Test for edge cases

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -11,20 +11,24 @@ RSpec.describe 'As a visitor' do
       contestant1 = Contestant.create!(name: "Kentaro Kameyama", age: 32, hometown: "Denver, CO", years_of_experience: 10)
       contestant2 = Contestant.create!(name: "Jay McCarroll", age: 37, hometown: "Austin, TX", years_of_experience: 15)
 
-      project1.contestants << contestant1
-      project2.contestants << contestant1
-      project3.contestants << contestant2
+      contestant1.projects << [project1, project2]
+      contestant2.projects << project3
 
       visit '/contestants'
 
       within("#contestant-#{contestant1.id}") do
         expect(page).to have_content(contestant1.name)
         expect(page).to have_content("Projects: #{project1.name}, #{project2.name}")
+        expect(page).to_not have_content(contestant2.name)
+        expect(page).to_not have_content(project3.name)
       end
 
       within("#contestant-#{contestant2.id}") do
         expect(page).to have_content(contestant2.name)
         expect(page).to have_content("Projects: #{project3.name}")
+        expect(page).to_not have_content(contestant1.name)
+        expect(page).to_not have_content(project1.name)
+        expect(page).to_not have_content(project2.name)
       end
     end
   end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -2,23 +2,35 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor' do
   describe 'from the project show page' do
-    it "I can see that project's name, material, theme, and contestant count" do
+    it "I can see that project's name, material, theme, contestant count, and average experience" do
       challenge1 = Challenge.create!(theme: "Apartment Furnishings", project_budget: 50)
       project1 = Project.create!(name: "Litfit", material: "Lamp Shade", challenge: challenge1)
       contestant1 = Contestant.create!(name: "Kentaro Kameyama", age: 32, hometown: "Denver, CO", years_of_experience: 10)
       contestant2 = Contestant.create!(name: "Jay McCarroll", age: 37, hometown: "Austin, TX", years_of_experience: 15)
       contestant3 = Contestant.create!(name: "Ambrose Lau", age: 27, hometown: "San Diego, CA", years_of_experience: 6)
 
-      project1.contestants << contestant1
-      project1.contestants << contestant2
-      project1.contestants << contestant3
+      project1.contestants << [contestant1, contestant2, contestant3]
 
       visit "/projects/#{project1.id}"
+
       expect(page).to have_content("Litfit")
       expect(page).to have_content("Material: Lamp Shade")
       expect(page).to have_content("Challenge Theme: Apartment Furnishings")
       expect(page).to have_content("Number of Contestants: 3")
       expect(page).to have_content("Average Contestant Experience: 10.33")
+    end
+
+    it "I can't see average contestant experience if there are no contestants" do
+      challenge1 = Challenge.create!(theme: "Apartment Furnishings", project_budget: 50)
+      project1 = Project.create!(name: "Litfit", material: "Lamp Shade", challenge: challenge1)
+
+      visit "/projects/#{project1.id}"
+
+      expect(page).to have_content("Litfit")
+      expect(page).to have_content("Material: Lamp Shade")
+      expect(page).to have_content("Challenge Theme: Apartment Furnishings")
+      expect(page).to have_content("Number of Contestants: 0")
+      expect(page).to_not have_content("Average Contestant Experience:")
     end
 
     it "I can add a contestant to a project" do
@@ -33,6 +45,12 @@ RSpec.describe 'As a visitor' do
       project1.contestants << contestant2
       project1.contestants << contestant3
 
+      visit "/contestants"
+
+      within("#contestant-#{contestant4.id}") do
+        expect(page).to_not have_content("Projects: #{project1.name}")
+      end
+
       visit "/projects/#{project1.id}"
 
       expect(page).to have_content("Number of Contestants: 3")
@@ -40,6 +58,7 @@ RSpec.describe 'As a visitor' do
       fill_in "contestant_id", with: contestant4.id
       click_button "Add Contestant To Project"
 
+      expect(current_path).to eq("/projects/#{project1.id}")
       expect(page).to have_content("Number of Contestants: 4")
 
       visit "/contestants"
